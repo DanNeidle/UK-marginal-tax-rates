5a919af23e8bdc557944febf2d43487fb760fa3e
luccazade <la.wetter.sanchez@gmail.com>
2022-10-11 16:29:06 +0100

    Formatted local app

--- app.py
+++ app.py
@@ -1,61 +1,141 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-from dash import Dash, dcc, html
-from dash.dependencies import Input, Output
+from dash import Dash, dcc, html, dash_table, Input, Output, callback
+import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import ThemeChangerAIO, template_from_url
 import main as calcs
 import plotly.express as px
 import pandas as pd
 
 df = calcs.genMarginals(children=3, married=True, studentLoan=False)
 
-app = Dash(__name__)
+dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
 
-app.layout = html.Div([
-    html.H1(
-        children='UK marginal tax rate for married earner, family of 3 kids', style={
-            'textAlign': 'center'},
-        id='title'
-    ),
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc_css])
 
-    html.H2(children='It\'s insane.', style={
-        'textAlign': 'center'
-    }),
+header = html.H4(
+    "UK marginal tax rate calculator", className="bg-primary text-white p-2 mb-2 text-center"
+)
 
-    html.Div([
+married_dropdown = html.Div(
+    [
+        dbc.Label("Marital status"),
+        dcc.Dropdown(
+            ["Married", "Not married (incl. divorced or widowed)"],
+            "Not married (incl. divorced or widowed)",
+            id="married_drop",
+            clearable=False,
+        ),
+    ],
+    className="mb-4",
+)
 
-        html.Div([
-            html.Br(),
-            html.Label('Marital status'),
-            dcc.Dropdown(['Married', 'Not married (incl. divorced or widowed)'], 'Married',
-                         id='married_drop')
-        ], style={'width': '30%', 'display': 'inline-block'}),
+loan_dropdown = html.Div(
+    [
+        dbc.Label("Student loan"),
+        dcc.Dropdown(
+            ["No"],
+            "No",
+            id="student_loan",
+            clearable=False,
+        ),
+    ],
+    className="mb-4",
+)
 
-        html.Div([
-            html.Br(),
-            html.Label('Student loan?'),
-            dcc.Dropdown(['Yes', 'No'], 'Yes',
-                         id='student_loan')
-        ], style={'width': '30%', 'float': 'center', 'display': 'inline-block'}),
+children_dropdown = html.Div(
+    [
+        dbc.Label("Number of children"),
+        dcc.Dropdown(
+            {i: i for i in range(10)}, 0,
+            id='number_children',
+            clearable=False,
+        ),
+    ],
+    className="mb-4",
+)
 
-        html.Div([
-            html.Br(),
-            html.Label('Number of children'),
-            dcc.Dropdown({i: i for i in range(10)}, 0,
-                         id='number_children')
-        ], style={'width': '30%', 'display': 'inline-block'})
+line_graph = dcc.Graph(id='marginal-graph',
+                       # className="m-4"
+                       )
 
-    ]),
+controls = dbc.Card(
+    [married_dropdown,
+     loan_dropdown,
+     children_dropdown],
+    body=True,
+)
 
-    dcc.Graph(id='marginal-graph'
-              )
+app.layout = dbc.Container(
+    [
+        header,
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        controls,
+                        # When running this app locally, un-comment this line:
+                        # ThemeChangerAIO(aio_id="theme")
+                    ],
+                    width=4,
+                ),
+                dbc.Col(line_graph, width=8),
+            ]
+        ),
+    ],
+    fluid=True,
+    className="dbc",
+)
 
-], style={'display': 'flex', 'flex-direction': 'column'})
+
+# app.layout = html.Div([
+#     # html.H1(
+#     #     children='UK marginal tax rate calculator', style={
+#     #         'textAlign': 'left'},
+#     #     id='title'
+#     # ),
+#
+#     # html.H2(children='It\'s much higher than you might think', style={
+#     #     'textAlign': 'left'
+#     # }),
+#
+#     html.Div([
+#
+#         html.Div([
+#             html.Br(),
+#             dbc.Label("Marital status"),
+#             # html.Label('Marital status'),
+#             dcc.Dropdown(['Married', 'Not married (incl. divorced or widowed)'], 'Married',
+#                          id='married_drop')
+#         ],
+#             # style={'width': '30%', 'display': 'inline-block'}
+#         ),
+#
+#         html.Div([
+#             html.Br(),
+#             html.Label('Student loan?'),
+#             dcc.Dropdown(['Yes', 'No'], 'Yes',
+#                          id='student_loan')
+#         ], style={'width': '30%', 'float': 'center', 'display': 'inline-block'}),
+#
+#         html.Div([
+#             html.Br(),
+#             html.Label('Number of children'),
+#             dcc.Dropdown({i: i for i in range(10)}, 0,
+#                          id='number_children')
+#         ], style={'width': '30%', 'display': 'inline-block'})
+#
+#     ]),
+#
+#     dcc.Graph(id='marginal-graph'
+#               )
+#
+# ], style={'display': 'flex', 'flex-direction': 'column'})
 
 
 @app.callback(
     Output(component_id='marginal-graph', component_property='figure'),
-    # Output(component_id='title', component_property='value'),
     Input(component_id='number_children', component_property='value'),
     Input(component_id='married_drop', component_property='value'),
     Input(component_id='student_loan', component_property='value'),
@@ -70,15 +150,17 @@ def update_graph(number_children, married_drop, student_loan):
         loan = True
     else:
         loan = False
+
     dff = calcs.genMarginals(int(number_children), married, loan)
-    fig = px.line(dff, x="Gross Income", y="Marginal Tax Rate")
+    fig = px.line(dff,
+                  x="Gross Income",
+                  y="Marginal Tax Rate",
+                  template="plotly"
+                  )
     fig.layout.yaxis.tickformat = ',.0%'
 
-    fig.update_layout(transition_duration=500)
     return fig
 
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
